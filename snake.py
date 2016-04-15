d4a9c701bccd20c447c162bc45de65e1a89dfb7f
Okonos <tepyzboj@gmail.com>
2016-04-15 21:26:14 +0000

    Movement, no input, no collisions

--- snake.py
+++ snake.py
@@ -3,41 +3,61 @@ from time import sleep
 
 device = led.matrix()
 
-class Segment:
-	def __init__(self, x, y):
-		self.x = x
-		self.y = y
-
-	def getCoords(self):
-		return (self.x, self.y)
-
-	def setCoords(self, x, y):
-		self.x = x
-		self.y = y
-
 def setPixel(x, y):
 	device.pixel(x, y, 1)
 
 #def unset??
 
-segments = []
-segments.append(Segment(2, 4))
-segments.append(Segment(2, 3))
-segments.append(Segment(2, 2))
+class Snake:
+	class Direction:
+		UP = 0
+		RIGHT = 1
+		DOWN = 2
+		LEFT = 3
+
+	class Segment:
+		def __init__(self, x, y):
+			self.x = x
+			self.y = y
+
+		def getCoords(self):
+			return (self.x, self.y)
+
+		def setCoords(self, x, y):
+			self.x = x
+			self.y = y
+	
+	def __init__(self):
+		self.direction = self.Direction.DOWN
+		self.segments = []
+		self.segments.append(self.Segment(4, 3))
+		self.segments.append(self.Segment(3, 3))
+		self.segments.append(self.Segment(2, 3))
+
+	def draw(self):
+		for s in self.segments:
+			setPixel(*(s.getCoords()))
+	
+	def move(self):
+		head = self.Segment(*self.segments[0].getCoords())
+		self.segments.insert(0, self.segments.pop())
+
+		if self.direction == self.Direction.UP:
+			self.segments[0] = self.Segment(head.x, head.y-1)
+		elif self.direction == self.Direction.RIGHT:
+			self.segments[0].x = self.Segment(head.x+1, head.y)	
+		elif self.direction == self.Direction.DOWN:
+			self.segments[0] = self.Segment(head.x, head.y+1)
+		elif self.direction == self.Direction.LEFT:
+			self.segments[0] = self.Segment(head.x-1, head.y)
+
+
+snake = Snake()
 
 while True:
-	for s in segments:
-		setPixel(*(s.getCoords()))
-
+	snake.draw()
 	sleep(1)
-	#class Point?
-	#headCoords = segments[0].getCoords()
-	segments.insert(0, segments.pop())
-	#coords = segments[0].getCoords()
-	#coords[1] += 1
-	#segments[0].setCoords(*coords)
-	segments[0].y = segments[1].y + 1
-
+	snake.move()
 	device.clear()
 
 

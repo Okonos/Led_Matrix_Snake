4cb3cc9dd43e9c0f18ca9ab9f56f7babbaad2500
Okonos <tepyzboj@gmail.com>
2016-04-17 12:50:55 +0000

    Button movement using callbacks

--- snake.py
+++ snake.py
@@ -1,12 +1,30 @@
 import max7219.led as led
+import RPi.GPIO as GPIO
 from time import sleep
 
 device = led.matrix()
+GPIO.setmode(GPIO.BOARD)
 
 def setPixel(x, y):
 	device.pixel(x, y, 1)
 
 #def unset??
+
+def upCallback(channel):
+	if snake.direction != Snake.Direction.DOWN:
+		snake.direction = Snake.Direction.UP
+
+def rightCallback(channel):
+	if snake.direction != Snake.Direction.LEFT:
+		snake.direction = Snake.Direction.RIGHT	
+
+def downCallback(channel):
+	if snake.direction != Snake.Direction.UP:
+		snake.direction = Snake.Direction.DOWN
+
+def leftCallback(channel):
+	if snake.direction != Snake.Direction.RIGHT:
+		snake.direction = Snake.Direction.LEFT
 
 class Snake:
 	class Direction:
@@ -45,7 +63,7 @@ class Snake:
 		if self.direction == self.Direction.UP:
 			self.segments[0] = self.Segment(head.x, head.y-1)
 		elif self.direction == self.Direction.RIGHT:
-			self.segments[0].x = self.Segment(head.x+1, head.y)	
+			self.segments[0] = self.Segment(head.x+1, head.y)	
 		elif self.direction == self.Direction.DOWN:
 			self.segments[0] = self.Segment(head.x, head.y+1)
 		elif self.direction == self.Direction.LEFT:
@@ -53,14 +71,27 @@ class Snake:
 
 
 snake = Snake()
+GPIO.setup(35, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+GPIO.setup(36, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+GPIO.setup(37, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+GPIO.setup(38, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+GPIO.add_event_detect(35, GPIO.RISING, callback=leftCallback,bouncetime=300)
+GPIO.add_event_detect(36, GPIO.RISING, callback=downCallback,bouncetime=300)
+GPIO.add_event_detect(37, GPIO.RISING, callback=upCallback,bouncetime=300)
+GPIO.add_event_detect(38, GPIO.RISING, callback=rightCallback,bouncetime=300)
 
-while True:
-	snake.draw()
-	sleep(1)
-	snake.move()
+try:
+	while True:
+		snake.draw()
+		sleep(1)
+		snake.move()
+		device.clear()
+except KeyboardInterrupt:
+	GPIO.cleanup()
+	sleep(3)
 	device.clear()
 
-
+GPIO.cleanup()
 sleep(3)
 device.clear()
 

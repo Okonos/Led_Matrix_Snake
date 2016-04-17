9a3992283a0374ebd7fce06bc7fc508247feeb7d
Okonos <tepyzboj@gmail.com>
2016-04-17 13:35:23 +0000

    Collisions

--- snake.py
+++ snake.py
@@ -47,17 +47,39 @@ class Snake:
 	
 	def __init__(self):
 		self.direction = self.Direction.DOWN
+		self.collided = False
 		self.segments = []
 		self.segments.append(self.Segment(4, 3))
 		self.segments.append(self.Segment(3, 3))
 		self.segments.append(self.Segment(2, 3))
+		self.segments.append(self.Segment(1, 3))
+		self.segments.append(self.Segment(0, 3))
 
 	def draw(self):
 		for s in self.segments:
 			setPixel(*(s.getCoords()))
+
+	def checkCollisions(self, head):
+		buffLen = len(device._buffer) - 1
+		# Wall collision
+		if (
+			head.y == 0 and self.direction == self.Direction.UP or
+			head.y == buffLen and self.direction == self.Direction.DOWN or
+			head.x == 0 and self.direction == self.Direction.LEFT or
+			head.x == buffLen and self.direction == self.Direction.RIGHT
+			):
+			return True
+
+		return False
 	
 	def move(self):
 		head = self.Segment(*self.segments[0].getCoords())
+
+		if self.checkCollisions(head):
+			self.collided = True
+			device.show_message("Dupa!")
+			return
+
 		self.segments.insert(0, self.segments.pop())
 
 		if self.direction == self.Direction.UP:
@@ -68,6 +90,11 @@ class Snake:
 			self.segments[0] = self.Segment(head.x, head.y+1)
 		elif self.direction == self.Direction.LEFT:
 			self.segments[0] = self.Segment(head.x-1, head.y)
+
+		# Segments collision
+		for segment in self.segments[1:]:
+			if self.segments[0].getCoords() == segment.getCoords():
+				self.collided = True
 
 
 snake = Snake()
@@ -81,11 +108,11 @@ GPIO.add_event_detect(37, GPIO.RISING, callback=upCallback,bouncetime=300)
 GPIO.add_event_detect(38, GPIO.RISING, callback=rightCallback,bouncetime=300)
 
 try:
-	while True:
+	while not snake.collided:
+		device.clear()
 		snake.draw()
 		sleep(1)
 		snake.move()
-		device.clear()
 except KeyboardInterrupt:
 	GPIO.cleanup()
 	sleep(3)
